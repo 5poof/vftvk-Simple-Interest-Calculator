3b56b4390265e0c7986694f879a94fd775b55388
Spoof the Strawman <80165056+5poof@users.noreply.github.com>
2021-04-01 15:13:46 -0500

    update.js
    added display slider value

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var slider = document.getElementById("rate");
+var display = document.getElementById("display");
+display.innerHTML = slider.value;
+
+slider.oninput = function() {
+    display.innerHTML = this.value;
+}
+
 function compute()
 {
     p = document.getElementById("principal").value;
